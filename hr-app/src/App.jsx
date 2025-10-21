c526c8b40c9897ac1c61231f1ea6affde3e7bccc
Eleonora <eleonora.kopiika@gmail.com>
2025-10-21 14:29:39 +0300

    Initial commit

---
+++ hr-app/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function Card(props) {
+  return (
+  <div className="card"> 
+  <h2>{props.name}</h2>
+  <div>Title: {props.title}</div>
+  <div>Age: {props.age}</div> </div>
+  );
+  }
+  
+
+function App() {
+
+  return (
+    <>
+    <div className="header">
+      <p>Logo</p>
+    </div>
+      <div className='main'>
+        <Card name ="Maria" title="CEO" age="29"/>
+        <Card name ="Kati" title="Developer" age = "25"/>
+        <Card name ="Katrin" title="Designer" age = "45"/>
+      </div>
+    <div className="footer">Copyrights</div>
+    </>
+  )
+}
+
+export default App
