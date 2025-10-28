99264485de634ab4314e9b4bd879bf14c50179be
Eleonora <eleonora.kopiika@gmail.com>
2025-10-28 14:59:27 +0200

    end of lesson 2

---
+++ new-app/src/data.js
@@ -0,0 +1,17 @@
+export default [
+	{
+		name:"Maria",
+		title:"CEO",
+		age: 29,
+	},
+	{
+		name:"Kati",
+		title:"Developer",
+		age: 25,
+	},
+	{
+		name:"Karin",
+		title:"Designer",
+		age: 45,
+	},
+];
---
+++ new-app/src/hooks/useCounter.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+	const useCounter = () =>{
+		const [count, setCount] = useState(0);
+		return {count, setCount}
+	}
+
+export default useCounter;
